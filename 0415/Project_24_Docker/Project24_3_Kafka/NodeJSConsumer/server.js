3590e8a4b4cad7d1c76a7f5c7ea140812562c0c7
adam <adam.st.wang@viewsonic.com>
2025-04-14 23:34:42 +0800

    feat: Add 24

--- 0415/Project_24_Docker/Project24_3_Kafka/NodeJSConsumer/server.js
+++ 0415/Project_24_Docker/Project24_3_Kafka/NodeJSConsumer/server.js
@@ -1,33 +1,31 @@
-const express = require('express');
+const express = require("express");
 const consumer = require("./myconsumer");
-const app = express()
+const app = express();
 
 app.use(express.json());
 let options = {
-    dotfiles: "ignore",
-    redirect:false
-}
+  dotfiles: "ignore",
+  redirect: false,
+};
 
-app.use(express.static('public',options))
+app.use(express.static("public", options));
 
-app.get("/",(req,res)=>{
-    var response = `
+app.get("/", (req, res) => {
+  var response = `
     <html>
         <h1>Confluence Kafka REST</h1>
         <div>
         <form action="/consumer" method="get" >
-            <input type="submit" value="????"/>
+            <input type="submit" value="Consume Vehicle Coordinates"/>
         </form>
         </div>
-    </html>`
-res.send(response)
-}
-)
-app.get("/consumer", (req,res)=>{
-    let topic = "????"
-    response = consumer.myconsumer({topic:topic})
-    res.send("Success- Consumed: " +topic);
-})
+    </html>`;
+  res.send(response);
+});
+app.get("/consumer", (req, res) => {
+  let topic = "vehicle-coordinates";
+  response = consumer.myconsumer({ topic: topic });
+  res.send("Success- Consumed: " + topic);
+});
 
-
-app.listen(5000,()=>console.log('Listening on 5000'))
+app.listen(5000, () => console.log("Listening on 5000"));
