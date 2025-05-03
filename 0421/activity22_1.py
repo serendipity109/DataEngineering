3a0fa80f74b90c254941c1201d08dce118fcdcbf
serendipity109 <jayda960825@gmail.com>
2025-05-04 00:21:20 +0800

    feat: Add 22.1

--- 0421/activity22_1.py
+++ 0421/activity22_1.py
@@ -1,14 +1,17 @@
 import pandas as pd
 from dask import dataframe as dd
 
-#create pandas dataframe
+odd_numbers = list(range(1, 11, 2))
+even_numbers = list(range(2, 11, 2))
 
+pandas_df = pd.DataFrame({'odd_num': odd_numbers, 'even_num': even_numbers})
 
-#set npartition argument
-df = dd.from_pandas(pandas_df)
+print(pandas_df)
 
-#Uncomment the line below if you are using Mac
-#df.to_csv("./activity22.1/", index=False)
+dask_df = dd.from_pandas(pandas_df, npartitions=2)
 
-#Uncomment the line below if you are using Windows
-#df.to_csv("C:/tmp/mo-pcde/activity22.1/", index=False)
+# Uncomment the line below if you are using Mac
+dask_df.to_csv("./activity22.1/", index=False)
+
+# Uncomment the line below if you are using Windows
+# df.to_csv("C:/tmp/mo-pcde/activity22.1/", index=False)
